8ddc7163a906c6593dad8a955a655eaf10ef2c6d
Hendrik Praditya <pradityahendrik@gmail.com>
2020-07-17 22:22:38 +0700

    init

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routes for WEB
+var indexRouter = require('./routes/web/index');
+var usersRouter = require('./routes/web/users');
+
+// Routes for API
+var indexApiRouter = require('./routes/api/index');
 
 var app = express();
 
@@ -19,8 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// WEB
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// API
+app.use('/api', indexApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
