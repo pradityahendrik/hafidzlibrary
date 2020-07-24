74500d7f13fcf9120e311ab91481e7d6d0d8c84a
Hendrik Praditya <pradityahendrik@gmail.com>
2020-07-24 10:56:43 +0700

    update

--- public/javascripts/category.js
+++ public/javascripts/category.js
@@ -4,10 +4,12 @@ $(document).ready(async function () {
         $('#listCategoryLoader').remove();
         data.data.forEach(row => {
             $('#listCategory').append(`
-                <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">`+
-                    row.name
-                    +`<span class="badge badge-secondary badge-pill">`+row.total+`</span>
-                </li>
+                <a href="">
+                    <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">`+
+                        row.name
+                        +`<span class="badge badge-secondary badge-pill">`+row.total+`</span>
+                    </li>
+                </a>
             `);
             $('.list-data').text(row.name)
         });
