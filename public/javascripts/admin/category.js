27b33808d6da3bfd42aafea6598bf8f136434639
Hendrik Praditya <pradityahendrik@gmail.com>
2021-04-18 14:45:56 +0700

    admin

--- public/javascripts/admin/category.js
+++ public/javascripts/admin/category.js
@@ -1,12 +1,32 @@
 $(document).ready(async function () {
-    try {
-        const query = {
-            page: 1,
-            limit: 10
-        }
-        const list = await listCategory(query);
-        console.log(list);
-    } catch (error) {
-        console.log(error);
+    const query = {
+        page: 1,
+        limit: 10
     }
+
+    const fetchCategory = async payload => {
+        try {
+            const category = await listCategory(payload);
+            category.data.data.forEach((row, index) => {
+                $('#listCategoryTable').append(`
+                    <tr>
+                        <td>${index+1}</td>
+                        <td>${row.Name}</td>
+                        <td>
+                            <a href="/edit/${row.Id}" class="btn btn-success" style="color: white">
+                                <span class="fa fa-edit"></span>
+                            </a>
+                            <a href="/delete/${row.Id}" class="btn btn-danger" style="color: white">
+                                <span class="fa fa-trash"></span>
+                            </a>
+                        </td>
+                    </tr>
+                `);
+            });
+        } catch (error) {
+          console.log(error);
+        }
+    }
+
+    fetchCategory(query);
 })
